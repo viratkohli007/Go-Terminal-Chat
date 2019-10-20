dc8bd36f1fd27952984a7117f95c4fbb23edb5c1
Aniket <aniket.amin@bacancytechnology.com>
2019-10-20 17:46:56 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+	"bufio"
+	"os"
+)
+
+var upgrader websocket.Upgrader
+var dialer = websocket.Dialer{}
+var header http.Header
+
+func main(){
+	fmt.Println("Hello websockets")
+
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	http.HandleFunc("/ws", handler)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatal("Error in creating server")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+ //    conn2, _, err := dialer.Dial("ws://localhost:8082/ws", header)
+	// if err != nil {
+	// 		    log.Println(err)
+	// 		    return
+	// 		}
+    for {
+    	// fmt.Scanf("Write your message: ")
+    	var input string
+    	fmt.Println("Enter server message:")
+    	// fmt.Scanln(&input)	
+    	scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+		    input = scanner.Text()
+		    // fmt.Printf("Input was: %q\n", line)
+		}
+    	err = conn.WriteMessage(1, []byte(input))
+        if err != nil {
+        	fmt.Println("err-->", err)
+        } else{
+        
+		 		
+				
+				// fmt.Println(conn)
+				// fmt.Println(htpResp)
+				
+				_, msgByte, err := conn.ReadMessage()
+				if err != nil {
+			 		fmt.Println("error in reading " , err)
+			 	}
+			 	fmt.Println("messages from client---->",string(msgByte))
+			 	// if err := conn2.Close(); err != nil {
+			 	// 	fmt.Println("error in closing")
+			 	// }
+			 	
+				
+	}
+}
+   // ... Use conn to send and receive messages.
+ 	msgType, msgByte, err := conn.ReadMessage()
+ 	fmt.Println(string(msgByte))
+ 	fmt.Println(msgType)
+ 	if err = conn.WriteMessage(1, []byte("hello from server")); err != nil {
+ 		fmt.Println("err-->", err)
+ 	}
+}
---
+++ websocket.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+	// "reflect"
+	"bufio"
+	"os"
+)
+
+var upgrader websocket.Upgrader
+
+var dialer = websocket.Dialer{}
+var header http.Header
+var globalConn *websocket.Conn
+
+func main(){
+	fmt.Println("Hello websockets222222")
+	conn, _, err := dialer.Dial("ws://localhost:8081/ws", header)
+	if err != nil {
+	    log.Println(err)
+	    return
+	}
+	// fmt.Println(reflect.TypeOf(conn))
+	globalConn = conn
+	// upgrader.CheckOrigin = func(r *http.Request) bool {
+	// 	return true
+	// }
+	// http.HandleFunc("/ws", handler)
+	// err := http.ListenAndServe(":8082", nil)
+	// if err != nil {
+	// 	log.Fatal("Error in creating server")
+	// }
+	
+		// fmt.Println(conn)
+		// fmt.Println(htpResp)
+		_, msgByte, err := conn.ReadMessage()
+		if err != nil {
+	 		fmt.Println("error in reading " , err)
+	 	}
+	 	fmt.Println("messages from server---->",string(msgByte))
+		handler()
+	// http.HandleFunc("/ws", handler)
+	if err := http.ListenAndServe(":8082", nil); err != nil{
+		fmt.Println("conn errror", err)
+	}
+
+}
+
+func handler() {
+	// conn, err := upgrader.Upgrade(w, r, nil)
+     
+	for {
+		
+	 	var input string
+	 	fmt.Println("type msg: ")
+	 	// fmt.Scanln(&input)
+	 	scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+		    input = scanner.Text()
+		    // fmt.Printf("Input was: %q\n", line)
+		}
+	 	err := globalConn.WriteMessage(1, []byte(input))
+        if err != nil {
+        	fmt.Println("err-->", err)
+        }
+        _, msgByte, err := globalConn.ReadMessage()
+		if err != nil {
+	 		fmt.Println("error in reading " , err)
+	 	}
+	 	fmt.Println("messages from server---->",string(msgByte))
+	}
+}
+   // ... Use conn to send and receive messages.
+ 	// msgType, msgByte, err := conn.ReadMessage()
+ 	// fmt.Println(string(msgByte))
+ 	// fmt.Println(msgType)
+ 	// if err = conn.WriteMessage(msgType, []byte("hello from server")); err != nil {
+ 	// 	fmt.Println("err-->", err)
+ 	// }
